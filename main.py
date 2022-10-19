55a75f6a4d49002c06c42a1286e367be9be260d4
yyxyy <47000546+Swinggger@users.noreply.github.com>
2022-10-18 17:54:23 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,28 @@
-import numpy as np
-#global variables
-BOARD_ROWS = 5
-BOARD_COLS = 5
-ACTION_N = 5
-P_E = 0.5
+# This is a sample Python script.
 
-stateT = np.array([[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4]])
-stateValues = np.zeros([BOARD_ROWS, BOARD_COLS])
-action = [[0, 1], [0, -1], [-1, 0], [1, 0], [0, 0]]
-tranProb = {
-    (tuple(stateT[0]), tuple(stateT[0]), tuple(action[0])): 0
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+    #print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import mdptoolbox.example
+P, R = mdptoolbox.example.forest()
+vi = mdptoolbox.mdp.ValueIteration(P, R, 0.9)
+vi.run()
+vi.policy # result is (0, 0, 0)
+print(vi.policy)
+
+class
+
+def rewardfunction(){
+
 }
-
-def isObsticle(a):
-    if np.all(a == [1, 1]) or np.all(a == [2, 1]) or np.all(a == [1, 3]) or np.all(a == [2, 3]):
-        return 1
-    else:
-        return 0
-
-def isNeighbour(s, s_p):
-
-    x = s + np.array([0, 1])
-    y = s + np.array([0, -1])
-    z = s + np.array([1, 0])
-    w = s + np.array([-1, 0])
-
-    if np.all(s_p == x) or np.all(s_p == y) or np.all(s_p == z) or np.all(s_p == w):
-        if isObsticle(s_p) == 1:
-            return 0
-        else:
-            return 1
-    else:
-        return 0
-
-#funtion to calculate transition probabilities
-def calculate_TP(state):
-
-    for i in state:
-        for a in action:
-            for j in state:
-                if a == [0, 0]:
-                    if np.all(i == j):
-                        tranProb[(tuple(i), tuple(j), tuple(a))] = 1
-                    else:
-                        tranProb[(tuple(i), tuple(j), tuple(a))] = 0
-                else:
-                    if isNeighbour(i, j) == 1:
-                        ai = i+a
-                        if np.array_equal(j, ai):
-                            tranProb[(tuple(i), tuple(j), tuple(a))] = 1 - P_E
-                        else:
-                            tranProb[(tuple(i), tuple(j), tuple(a))] = P_E/4
-                    else:
-                        tranProb[(tuple(i), tuple(j), tuple(a))] = 0
-
-    for s in state:
-        for a in action:
-            if a != [0, 0]:
-                summ = 0
-                for n in state:
-                    summ = summ + tranProb[(tuple(s), tuple(n), tuple(a))]
-                tranProb[(tuple(s), tuple(s), tuple(a))] = 1 - summ
-
-if __name__ == "__main__":
-    calculate_TP(stateT)
-    print(tranProb)
-
-
-
-
